be3142be2799550e7f9df97b28698ef8ec35a194
Anderson GM <anderson.milham@outlook.com>
2016-12-31 11:49:31 -0200

    Module 1 Coding Assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict'
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+
+        $scope.dishes = "";
+        // $scope.message = "";
+
+        $scope.check = function () {
+
+            if (!$scope.dishes) {
+
+                $scope.message = "Please enter data first";
+
+            } else {
+
+                var numberOfDishes = $scope.dishes.split(",").length;
+
+                if (numberOfDishes <= 3) {
+                    $scope.message = "Enjoy!";
+                } else {
+                    $scope.message = "Too much!";
+                }
+
+            }
+
+        };
+
+    }
+
+})();
